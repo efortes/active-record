0ea6cef47f95cda6992893d052842c0be87a3714
Elvin Fortes <elvin087@gmail.com>
2015-03-10 17:08:16 +0100

    test

--- index.js
+++ index.js
@@ -1,41 +1,7 @@
-
-var MasterModel = require("./lib/master-model");
-//
-//class User extends MasterModel {
-//	
-//  constructor(data) {
-//    super(data, {
-//    	adapter: "mysql",
-//    	connection: null
-//    });
-//    
-//    var fields = [MasterModel.createField({
-//	    	name: "userName", 
-//	    	scimAttr: "scimUserName",
-//	    	defaultValue: "My user name",
-//	  	}), MasterModel.createField({
-//	    	name: "name",
-//	    	convert: function(val, record) {
-//	    		return val + " is my name";
-//	    	},
-//	    	defaultValue: "My name",
-//	  	}), MasterModel.createField({
-//	    	name: "gender",
-//	    	mandatory: true,
-//	  	})  
-//  	];
-//    
-//    this.init(data, fields);    
-//  }
-//  
-//}
-//
-//
-//var user = new User({userName: "efortes", name: "Elvin Fortes"});
-//console.log(user.get("name"));
-//console.log(user.validate());
-//console.log(user.getData());
-//console.log(user.getFields());
-//console.log("Is valid: " + user.isValid());
-
-module.exports = MasterModel;
+var traceur = require('traceur');
+require('traceur-source-maps').install(traceur);
+traceur.require.makeDefault(function(filename) {
+  // don't transpile our dependencies, just our app
+  return filename.indexOf('node_modules') === -1;
+});
+module.exports = require("./index");
