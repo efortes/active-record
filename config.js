10621d78ba4bbfc1caaf31ea20eded4de83a2d4f
Elvin Fortes <elvin087@gmail.com>
2016-10-06 14:37:30 +0200

    Mysql and ldap client added, bug fixes

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  logQuery: false
+  logQuery: false,
+  ldapIgnoreSelfSignedCertificates: true //
 }
--- index.js
+++ index.js
@@ -3,13 +3,50 @@
 const MasterModel = require('./lib/master-model');
 const SqlModel = require('./lib/sql-model');
 const LdapModel = require('./lib/ldap-model');
+const LdapClient = require('./lib/client/ldap-client');
+const MysqlClient = require('./lib/client/mysql-client');
 const config = require('./config');
 
 module.exports = {
+
+  /**
+   * Master model
+   */
   MasterModel: MasterModel,
+
+  /**
+   * Mysql model
+   */
   SqlModel: SqlModel,
+
+  /**
+   * Ldap model
+   */
   LdapModel: LdapModel,
+
+  /**
+   * Override default module config
+   * @param newConfig
+   */
   setConfig: (newConfig) => {
     Object.assign(config, newConfig);
+  },
+
+  /**
+   * See https://www.npmjs.com/package/mysql#pool-options for the mysql pool options
+   * @param <Object> sqlConfig
+   * @returns {Pool}
+   */
+  createLdapConn(ldapConfig) {
+    return new LdapClient(ldapConfig);
+  },
+
+  /**
+   * See https://www.npmjs.com/package/mysql#pool-options for the mysql pool options
+   * @param <Object> sqlConfig
+   * @returns {Pool}
+   */
+  createMysqlConn(sqlConfig) {
+    return new MysqlClient(ldapConfig);
   }
 };
